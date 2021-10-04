18474ab9d49d4c8d9981946621eb55536506a6f5
Kaku Tatsunori <gameprogramer1993@gmail.com>
2021-10-04 22:02:38 +0900

    『18. Console Commands With Exec』まで。GameInstanceのInitが呼ばれなかったのはProjectSettingsでの指定が間違っていただけだった。

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -3,6 +3,8 @@
 
 #include "PuzzlePlatformsGameInstance.h"
 
+#include "Engine.h"
+
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
 {
@@ -15,4 +17,30 @@ void UPuzzlePlatformsGameInstance::Init()
 	Super::Init();
 
 	UE_LOG(LogTemp, Warning, TEXT("UPuzzlePlatformsGameInstance Init"));
+}
+
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	check(Engine);
+	if (!Engine)
+	{
+		return;
+	}
+
+	Engine->AddOnScreenDebugMessage(INDEX_NONE, 2.0f, FColor::Green, TEXT("Hosting"));
+}
+
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	check(Engine);
+	if (!Engine)
+	{
+		return;
+	}
+
+	Engine->AddOnScreenDebugMessage(INDEX_NONE, 5.0f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
 }
